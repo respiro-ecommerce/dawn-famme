8f002f4e5b0e95c3b15057b3b8d57fa73a59466e
GT <gregjotau@gmail.com>
2023-04-22 19:06:09 +0200

    test sync with dawn upstream

--- assets/animations.js
+++ assets/animations.js
@@ -6,25 +6,33 @@ function onIntersection(elements, observer) {
     if (element.isIntersecting) {
       const elementTarget = element.target;
       elementTarget.classList.add(SCROLL_ANIMATION_ACTIVE_CLASSNAME);
-      if (elementTarget.hasAttribute("data-cascade"))
+      if (elementTarget.hasAttribute('data-cascade'))
         elementTarget.setAttribute('style', `--animation-order: ${index};`);
       observer.unobserve(elementTarget);
     }
-  })
+  });
 }
 
-function initializeScrollAnimationTrigger(rootEl = document) {
-  const animationTriggerElements = Array.from(
-    rootEl.getElementsByClassName(SCROLL_ANIMATION_TRIGGER_CLASSNAME)
-  );
+function initializeScrollAnimationTrigger(rootEl = document, isDesignModeEvent = false) {
+  const animationTriggerElements = Array.from(rootEl.getElementsByClassName(SCROLL_ANIMATION_TRIGGER_CLASSNAME));
   if (animationTriggerElements.length === 0) return;
 
+  if (isDesignModeEvent) {
+    animationTriggerElements.forEach((element) => {
+      element.classList.add('scroll-trigger--design-mode');
+    });
+    return;
+  }
+
   const observer = new IntersectionObserver(onIntersection, {
-    threshold: 0.1,
+    rootMargin: '0px 0px -50px 0px',
   });
   animationTriggerElements.forEach((element) => observer.observe(element));
 }
 
 window.addEventListener('DOMContentLoaded', () => initializeScrollAnimationTrigger());
 
-document.addEventListener('shopify:section:load', (event) => initializeScrollAnimationTrigger(event.target));
+if (Shopify.designMode) {
+  document.addEventListener('shopify:section:load', (event) => initializeScrollAnimationTrigger(event.target, true));
+  document.addEventListener('shopify:section:reorder', () => initializeScrollAnimationTrigger(document, true));
+}
